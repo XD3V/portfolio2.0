0ccc2e16e9309d7ce450fd201bf1e9eb59a02d88
Xavier Davis <xdavis1@icloud.com>
2020-03-13 11:14:59 -0700

    Updates

---
+++ precache-manifest.194918d715dbc68a5706673c22288079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f991228e37da6144d40fed3b7eacb9",
+    "url": "/XD3V/portfolio2.0/index.html"
+  },
+  {
+    "revision": "7e1aa71540af7401a596",
+    "url": "/XD3V/portfolio2.0/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3993deca0145b7b46d4b",
+    "url": "/XD3V/portfolio2.0/static/js/2.6aa10b31.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/XD3V/portfolio2.0/static/js/2.6aa10b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1aa71540af7401a596",
+    "url": "/XD3V/portfolio2.0/static/js/main.706fbc4c.chunk.js"
+  },
+  {
+    "revision": "ff9f6da7021641317568",
+    "url": "/XD3V/portfolio2.0/static/js/runtime-main.4a2db6a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XD3V/portfolio2.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
